c9e2ea3fe07fae6a2136cacc650c863a6559bddc
David LeVack <davidlevack@gmail.com>
2024-12-10 00:04:26 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 // app/layout.tsx
 import type { Metadata } from "next";
 import { AuthProvider } from "@/contexts/AuthContext";
-import "@/styles/globals.css";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Kizmet Dating App",
