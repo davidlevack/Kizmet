d909d3a7d142c7f04496e5d3b93181fad7cc1a0d
David LeVack <davidlevack@gmail.com>
2024-12-10 00:58:25 -0500

    1

--- components/ui/slider.tsx
+++ components/ui/slider.tsx
@@ -3,10 +3,10 @@
 import * as React from "react"
 import { cn } from "@/lib/utils"
 
-interface SliderProps extends React.InputHTMLAttributes<HTMLInputElement> {
+type SliderProps = Omit<React.InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange'> & {
   value?: number[];
   onValueChange?: (value: number[]) => void;
-}
+};
 
 const Slider = React.forwardRef<HTMLInputElement, SliderProps>(
   ({ className, value, onValueChange, ...props }, ref) => {
@@ -24,8 +24,15 @@ const Slider = React.forwardRef<HTMLInputElement, SliderProps>(
           onChange={handleChange}
           className={cn(
             "w-full h-2 bg-secondary rounded-full appearance-none cursor-pointer",
-            "range-input:h-5 range-input:w-5 range-input:rounded-full range-input:border-2",
-            "range-input:border-primary range-input:bg-background",
+            "[&::-webkit-slider-thumb]:appearance-none",
+            "[&::-webkit-slider-thumb]:h-5",
+            "[&::-webkit-slider-thumb]:w-5",
+            "[&::-webkit-slider-thumb]:rounded-full",
+            "[&::-webkit-slider-thumb]:bg-primary",
+            "[&::-moz-range-thumb]:h-5",
+            "[&::-moz-range-thumb]:w-5",
+            "[&::-moz-range-thumb]:rounded-full",
+            "[&::-moz-range-thumb]:bg-primary",
             "focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
             className
           )}
@@ -37,4 +44,4 @@ const Slider = React.forwardRef<HTMLInputElement, SliderProps>(
 )
 Slider.displayName = "Slider"
 
-export { Slider }
+export { Slider } 
